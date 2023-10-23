376442c97d48ad007a0d14c10478a66f8df77603
Kosiname <mac@Kosiname-MacBook-Air.local>
2023-10-23 20:42:38 +0100

    Worked on a part of the Hero-section.

--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { CSSProperties } from 'react'
 
-export const HeroSection = () => {
+
+export const HeroSection = (props: any) => {
+  const Gradient: CSSProperties = {
+      background: "linear-gradient(91deg, #5100BF 14.44%, #FFD855 119.53%)",
+      backgroundClip: "text",
+      WebkitBackgroundClip: "text",
+      WebkitTextFillColor: "transparent",
+  };
+  const GeneralText: CSSProperties = {
+    color: "#171717",
+    textAlign: "center",
+    fontFamily: "Big Shoulders Display, sans-serif", 
+    fontSize: 18,
+    fontStyle: "normal",
+    fontWeight: 500,
+    lineHeight: "normal", 
+    textTransform: "capitalize",
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+  };
+  const NextLine: CSSProperties = {
+    color: "rgba(0, 0, 0, 0.70)",
+    fontFamily: "Helvetica Neue",
+    fontSize: 24,
+    fontStyle: "normal",
+    fontWeight: 400,
+    lineHeight: "normal",
+  }
   return (
-    <div>HeroSection</div>
+    <div style={GeneralText}>
+      <h1>Creating A Space For Young People To <span style={Gradient}>Connect And Grow</span></h1>
+      <div style={{columnGap: 0}}>
+      <div><h3 style={NextLine}>A community of young people in tech connecting, and growing, with a mission to make an impact in nigeria and africa using technology</h3></div>
+      </div>
+    </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { CSSProperties } from "react";
 import Navbar from "~/components/Commons/Navbar";
 import FixedButton from "~/components/Home/FixedButton";
+import { HeroSection } from "~/components/Home/HeroSection";
 
 export default function Home() {
   const containerStyle : CSSProperties = {
@@ -12,6 +13,7 @@ export default function Home() {
       <main style={containerStyle}>
         <Navbar />
         <FixedButton text="Series D-Event coming up Dec12, 2023. Register now!" />
+        <HeroSection />
       </main>
     </>
   );
